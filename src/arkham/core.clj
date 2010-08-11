91d08c6edb46cca72b48c626c2eab74d5ef9de7e
Kevin Downey (hiredman) <redchin@gmail.com>
2010-08-10 22:29:38 -0700

    more special forms support

--- src/arkham/core.clj
+++ src/arkham/core.clj
@@ -1,69 +1,146 @@
 (ns arkham.core
-  (:use [clojure.contrib.macro-utils :only [mexpand-all]]))
+  (:use [clojure.contrib.macro-utils :only [mexpand-all]]
+        [clojure.contrib.java-utils :only [wall-hack-method]]))
+
+(defrecord SpecialFrame [tag args])
+
+(defmethod print-method SpecialFrame [o w]
+  (.write w "#=<recur>"))
 
 (defmulti meval (comp type second))
 
 (defmethod meval :default [[stack exp]]
   [stack exp])
 
-(defmethod meval clojure.lang.Symbol [[[top & _ :as stack] exp]]
+(defmethod meval clojure.lang.Symbol [[stack exp]]
   [stack
-   (if (contains? top exp)
-     (get top exp)
-     (if-let [v (ns-resolve *ns* exp)]
-       @v
-       (throw
-        (Exception.
-         (format
-          "Unable to resolve symbol: %s in this context" (name exp))))))])
+   (if (= exp '*STACK*)
+     stack
+     (if-let [bound  (->> stack
+                          (remove #(= SpecialFrame (type %)))
+                          (filter #(contains? % exp))
+                          first)]
+       (get bound exp)
+       (if-let [v (ns-resolve *ns* exp)]
+         @v
+         (throw
+          (Exception.
+           (format
+            "Unable to resolve symbol: %s in this context" (name exp)))))))])
 
 (defmulti eval-seq (fn [[stack [first & _]]] first))
 
 (defmethod meval clojure.lang.ISeq [[state exp]]
-  [state (eval-seq [state exp])])
+  (trampoline eval-seq [state exp]))
 
 (defmethod eval-seq :default [[state [op & args]]]
   (let [op (second (meval [state op]))
         args (doall (map (fn [x] (second (meval [state x]))) args))]
-    (apply op args)))
+    [state (apply op args)]))
 
 (defmethod eval-seq 'let* [[stack [_ bindings & body]]]
-  (second
-   (let [stack1 (conj stack (into {} (map vec (partition 2 bindings))))]
-     (meval
-      [stack1 (cons 'do body)]))))
+  [stack
+   (second
+    (let [stack1 (conj stack
+                       (reduce
+                        (fn [locals [name value]]
+                          (assoc locals
+                            name (second (meval [(conj stack locals) value]))))
+                        {}
+                        (partition 2 bindings)))]
+      (meval
+       [stack1 (cons 'do body)])))])
 
 (defmethod eval-seq 'do [[stack [_ & body]]]
-  (second
-   (last
-    (doall
-     (map (fn [e]
-            (meval [stack e]))
-          body)))))
+  [stack
+   (second
+    (last
+     (doall
+      (map (fn [e]
+             (meval [stack e]))
+           body))))])
 
-(defmethod eval-seq 'quote [[stack [_ & body]]]
-  (first body))
+(defmethod eval-seq 'quote [[stack [_ name]]]
+  [stack name])
 
 (defmethod eval-seq 'var [[stack [_ & body]]]
-  (ns-resolve *ns* (first body)))
+  [stack (ns-resolve *ns* (first body))])
+
+(defmethod eval-seq 'if [[stack [_ a b c]]]
+  [stack
+   (if (second (meval [stack a]))
+     (second (meval [stack b]))
+     (second (meval [stack c])))])
+
+(defmethod eval-seq '. [[stack [_ target name- & args]]]
+  [stack
+   (if (class? target)
+     (throw (Exception. "huh?"))
+     (clojure.lang.Reflector/invokeInstanceMethod
+      (second (meval [stack target]))
+      (name name-)
+      (into-array Object
+                  (map #(second (meval [stack %])) args))))])
+
+(defmethod eval-seq 'new [[stack [_ class & args]]]
+  [stack
+   (clojure.lang.Reflector/invokeConstructor
+    (second (meval [stack (list 'var class)]))
+    (into-array Object
+                (map #(second (meval [stack %])) args)))])
+
+(defmethod eval-seq 'throw [[stack [_ exception]]]
+  (throw (second (meval [stack exception]))))
+
+(defmethod eval-seq 'loop* [[stack [_ bindings & bodies]]]
+  [stack
+   (second
+    (meval
+     [(conj stack (SpecialFrame. :loop [(take-nth 2 bindings) bodies]))
+      (conj bodies bindings 'let*)]))])
+
+(defmethod eval-seq 'recur [[stack [_ & args]]]
+  (fn []
+    (let [stack1 (loop [[s & ss] stack]
+                  (if (not (and (= SpecialFrame (type s))
+                                (= :loop (:tag s))))
+                    (recur ss)
+                    (conj ss s)))
+          {tag :tag [bindings bodies] :args} (first stack1)
+          args (->> args
+                    (map #(meval [stack %]))
+                    (map second)
+                    (map (partial list 'quote)))
+          bindings (vec (mapcat list bindings args))]
+      (meval
+       [(rest stack1) (conj bodies bindings 'loop*)]))))
 
 (defmethod eval-seq 'fn* [[stack [_ & bodies]]]
-  (println stack bodies)
-  (let [max-args (if-let [m (->> bodies
-                                 (map first)
-                                 (map #(.contains % '&))
-                                 (some true?))]
-                   m
-                   (apply max (map count (map first bodies))))]
-    max-args)
+  [stack
+   (let [max-args (if-let [m (->> bodies
+                                  (map first)
+                                  (map #(.contains % '&))
+                                  (some true?))]
+                    m
+                    (apply max (map count (map first bodies))))]
+     max-args)]
   #_(fn [& args]
-    (second
-     (meval
-      [(conj stack (into {} (map vector bindings args)))
-       (cons 'do body)]))))
+      (second
+       (meval
+        [(conj stack (into {} (map vector bindings args)))
+         (cons 'do body)]))))
+
+(defmethod eval-seq 'def [[stack [_ name value]]]
+  (let [evalue (second (meval [stack value]))]
+    (alter-meta!
+     (clojure.lang.Var/intern *ns* name evalue true)
+     (constantly (meta name)))))
 
 #_(defmethod eval-seq 'loop [[stack [_ bindings & body]]]
-  )
+    )
 
 (defn evil [exp]
   (second (meval [() (mexpand-all exp)])))
+
+(defn evil* [exp]
+  (meval [() (mexpand-all exp)]))
--- test/arkham/core_test.clj
+++ test/arkham/core_test.clj
@@ -1,16 +1,45 @@
 (ns arkham.core-test
-  (:use [arkham.core])
-  (:use [clojure.test]))
+  (:use [arkham.core]
+        [clojure.test]))
 
 (deftest test-evil
-  (testing "symbol resolution"
+    (testing "symbol resolution"
     (is (thrown? Exception (evil 'f))))
   (testing "function calling"
-    (is (= 3 (evil '(+ 1 2)))))
+    (is (= 3
+           (evil '(+ 1 2)))))
   (testing "special forms"
-    ;; . def loop* recur if let* letfn* do fn* quote var set! try
-    ;; catch finally throw deftype* case* new reify*
-    (is (= 1 (evil '(do 2 1))))
-    (is (= 'a (evil '(quote a))))
-    (is (= #'+ (evil '(var +))))
-    (is (= 3 (evil '(let [x 1 y 2] (+ x y)))))))
+    ;; TODO:
+    ;; letfn* fn* set! try
+    ;; catch finally deftype* case* reify*
+    ;; DONE:
+    ;; . if let* do quote var new throw loop/recur def
+    (testing "do"
+      (is (= 1
+             (evil '(do 2 1))))
+      (is (= 2 (evil '(do 1 (+ 1 1))))))
+    (testing "quote"
+      (is (= 'a (evil '(quote a)))))
+    (testing "var"
+      (is (= #'+ (evil '(var +)))))
+    (testing "let*"
+      (is (= 5 (evil '(let* [x (+ 1 2) y 2] (+ x y))))))
+    (testing "if"
+      (is (= 4 (evil '(if (zero? (- 1 2)) 1 4)))))
+    (testing "."
+      (is (= 4 (evil '(.invoke + 2 2)))))
+    (testing "throw"
+      (is (thrown? IllegalArgumentException
+                   (evil '(throw (IllegalArgumentException. "foo"))))))
+    (testing "loop/recur"
+      (is (= :end (evil '(loop [x 1] (if (= x 2) :end (recur (inc x)))))))
+      (is (= [2 2 2 2]
+               (evil '(loop [x 0 s []]
+                        (if (= x 4)
+                          s
+                          (recur (inc x) (conj s (count *STACK*)))))))
+          "loop/recur uses a constant stack space"))
+    (testing "def"
+      (let [s (gensym)]
+        (evil (list 'def s 1))
+        (is (= @(ns-resolve *ns* s) 1))))))
